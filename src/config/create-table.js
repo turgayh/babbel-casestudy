e1fd975ac18ba233afd8fa1acf1c5066c146bdb0
Hakan Turgay <hakan.turgay@modanisa.com>
2022-09-14 11:22:18 +0300

    add courses and lessons table

--- src/config/create-table.js
+++ src/config/create-table.js
@@ -26,17 +26,44 @@ CREATE TABLE IF NOT EXISTS users_photo(
 const languagesTable = `CREATE TABLE IF NOT EXISTS "languages" (
     "id" SERIAL,
     "name" VARCHAR(50) NOT NULL,
-    "code" VARCHAR(50) NOT NULL,
+    "code" VARCHAR(50) NOT NULL UNIQUE,
 	created_on TIMESTAMP NOT NULL,
     PRIMARY KEY ("id")
 
 );`;
 
+const lessonsTable = `
+CREATE TABLE IF NOT EXISTS lessons(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    "name" VARCHAR(100) NOT NULL,
+    "lesson_text" VARCHAR(250) NOT NULL,
+    lang_id INT,
+    status BOOLEAN DEFAULT true,
+	created_on TIMESTAMP DEFAULT NOW()  NOT NULL ,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_customer
+       FOREIGN KEY(lang_id) 
+       REFERENCES languages(id)
+ );
+`;
+
+const courseTable = `CREATE TABLE IF NOT EXISTS courses(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    user_id INT,
+    img text NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_customer
+       FOREIGN KEY(user_id) 
+       REFERENCES users(id)
+ );`;
+
 const createTable = async () => {
-    await pool.query(usersTable);
-    await pool.query(usersPhotoTable);
-    await pool.query(languagesTable);
+  await pool.query(usersTable);
+  await pool.query(usersPhotoTable);
+  await pool.query(languagesTable);
+  await pool.query(lessonsTable);
+  await pool.query(courseTable);
+};
 
-}
-
-createTable()
+createTable();
